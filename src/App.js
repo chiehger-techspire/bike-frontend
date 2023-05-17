fa80bb3ed14301d2bb37926e3cd1cf1a268aba85
Vineeth Paul Geever <vineeth.geever@gmail.com>
2023-05-17 13:58:41 +0200

    Basic bike rental page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { BikeRental } from './components/bike-rental';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <BikeRental />
     </div>
   );
 }
